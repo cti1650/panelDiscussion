4053baad60262223b4b9667a3128085ecaf66b1a
cti1650 <cti1650@gmail.com>
2021-05-05 13:36:30 +0900

    Tailwind問題②

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: [
-    './components/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './public/**/*.html',
   ],
